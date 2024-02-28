3d8e98c78144d2f237264d2534c9205f57a13adb
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2024-02-28 12:15:56 -0500

    Create filecloud.sh

---
+++ filecloud.sh
@@ -0,0 +1,15 @@
+sudo curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
+ 
+sudo curl -fsSL https://repo.filecloudlabs.com/static/pgp/filecloud.asc | sudo gpg -o /usr/share/keyrings/filecloud.gpg --dearmor
+ 
+sudo echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+ 
+sudo echo "deb [ arch=amd64 signed-by=/usr/share/keyrings/filecloud.gpg ] https://repo.filecloudlabs.com/apt/ubuntu jammy/filecloud/23.232 main" | sudo tee /etc/apt/sources.list.d/filecloud.list
+ 
+sudo apt-get update -y
+ 
+sudo apt-get install apache2 mongodb-org pigz -y
+
+sudo apt install -y --no-install-recommends php8.2*
+
+sduo ACCEPT_EULA=Y  apt-get install filecloud -y
